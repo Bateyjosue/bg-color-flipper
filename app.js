6c5e75745fa46c845bbdab8d9e9604b23348a3a4
Bateyjosue <josuebatey19@gmail.com>
2023-06-17 11:43:05 +0200

    Style Root

---
+++ app.js
@@ -0,0 +1,8 @@
+const root = document.querySelector('.root');
+
+root.innerHTML = `
+  <h2 class="root__title">
+  Background Color: <span>${null}</span>
+  </h2>
+  <button class="root__button">Click Me</button>
+`;
